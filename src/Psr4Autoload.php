27cd73753e98d7e13f4ff2979f5eba2e46877c44
xunzhibin <xunzhibin@jnexpert.com>
2023-10-19 13:09:37 +0800

    psr4加载 更新

--- src/Psr4Autoload.php
+++ src/Psr4Autoload.php
@@ -9,59 +9,42 @@ namespace xzb;
 class Psr4Autoload
 {
     /**
-     * 命名空间 前缀
-     * 
-     * @var string
-     */
-    const NAMESPACE_PREFIX = 'App\\';
-
-    /**
-     * CodeIgniter 系统类前缀
-     * 
-     * @var string
-     */
-    const CI_CLASS_PREFIX = 'CI_';
-
-    /**
      * 注册
      * 
      * @param string $namesapcePerfix
      * @return void
      */
-    public static function boot($namespacePrefix = null)
+    public static function boot()
     {
-        if (! $namespacePrefix) {
-            $namespacePrefix = self::NAMESPACE_PREFIX;
-        }
-
-        $ciClassPrefix = self::CI_CLASS_PREFIX;
-
         // 注册指定的函数作为 __autoload 的实现
-        spl_autoload_register(function ($classname) use ($namespacePrefix, $ciClassPrefix) {
+        spl_autoload_register(function ($classname) {
+            // 命名空间 前缀
+            $namespacePrefix = 'App\\';
+            // CodeIgniter3 系统类 前缀
+            $ciClassPrefix = 'CI_';
+
             // 检测 前缀
             if (strncmp($classname, $namespacePrefix, strlen($namespacePrefix)) === 0) {
-                // 定位类的相对路径
-                $classFilePath = APPPATH . ltrim($classname, $namespacePrefix) . '.php';
+                // 路径
+                $filePath = APPPATH . ltrim($classname, $namespacePrefix) . '.php';
                 // 替换 分隔符
-                $classFilePath = str_replace('\\', DIRECTORY_SEPARATOR, $classFilePath);
+                $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $filePath);
 
                 // 文件存在
-                if (file_exists($classFilePath)) {
+                if (file_exists($filePath)) {
                     // 加载
-                    require $classFilePath;
+                    require $filePath;
                 }
             } else if (strncmp($classname, $ciClassPrefix, strlen($ciClassPrefix)) === 0) {
-                // 定位类的相对路径
-                $corePath = BASEPATH . 'core' . DIRECTORY_SEPARATOR;
-                $libraryPath = BASEPATH . 'libraries' . DIRECTORY_SEPARATOR;
+                foreach ([
+                    BASEPATH . 'core' . DIRECTORY_SEPARATOR,
+                    BASEPATH . 'libraries' . DIRECTORY_SEPARATOR,
+                ] as $path) {
+                    if (! file_exists($filePath = $path . ltrim($classname, $ciClassPrefix). '.php')) {
+                        continue;
+                    }
 
-                // CI核心类
-                if (file_exists($coreFilePath = $corePath . ltrim($classname, $ciClassPrefix). '.php')) {
-                    require $coreFilePath;
-                }
-                // CI类库
-                else if (file_exists($libraryFilePath = $libraryPath . ltrim($classname, $ciClassPrefix). '.php')) {
-                    require $libraryFilePath;
+                    require $filePath;
                 }
             }
         });
